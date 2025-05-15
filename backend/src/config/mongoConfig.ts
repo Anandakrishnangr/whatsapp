82008aee507d6a1c73104f483a6017eda451527b
anandakrishnangr <granandakrishnan@gmail.com>
2025-05-15 14:34:28 +0530

    update docker-compose configuration and enhance mongo connection error logging

--- backend/src/config/mongoConfig.ts
+++ backend/src/config/mongoConfig.ts
@@ -4,6 +4,7 @@ export const connectMongoose =async () => {
     return mongoose.connect(process.env.MONGO_URI??'').then((res) => {
         console.log("connected")
     }).catch((err) => {
+        console.log(err)
         console.log("err")
     })
 }
